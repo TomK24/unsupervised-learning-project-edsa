8e431d3f3ebe977d0b4d8d47db5d1d0dc693497a
Thomas Kenyon <t.k@hotmail.co.za>
2022-02-03 11:10:48 +0200

    Reordered pages

--- edsa_recommender.py
+++ edsa_recommender.py
@@ -45,7 +45,7 @@ def main():
 
     # DO NOT REMOVE the 'Recommender System' option below, however,
     # you are welcome to add more options to enrich your app.
-    page_options = ["Recommender System","Solution Overview", "Exploratory Data Analysis", "Hybrid Recommender"]
+    page_options = ["Recommender System", "Hybrid Recommender","Solution Overview", "Exploratory Data Analysis"]
 
     # -------------------------------------------------------------------
     # ----------- !! THIS CODE MUST NOT BE ALTERED !! -------------------
